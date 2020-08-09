998b6c9ba80f968a3b7efc8660b353a9ad47f80e
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2020-08-09 16:22:56 -0300

    Removendo comentários.

--- www/routes/web.php
+++ www/routes/web.php
@@ -17,23 +17,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
-
 Route::resource('client', '\CustomerManagement\Http\Controllers\ClientController');
 Route::get('clients/{data?}', '\CustomerManagement\Http\Controllers\ClientController@index')->name('clients.index');
 Route::get('client/edit/{id}', '\CustomerManagement\Http\Controllers\ClientController@edit');
-//Route::get('client/{id}','\CustomerManagement\Http\Controllers\ClientController@show');
-//Route::post('client', '\CustomerManagement\Http\Controllers\ClientController@store');
-//Route::put('client/{id}', '\CustomerManagement\Http\Controllers\ClientController@update')->name('client.update');
-//Route::delete('client/{id}', '\CustomerManagement\Http\Controllers\ClientController@destroy')->name('client.destroy');
-//Route::get('client/create', '\CustomerManagement\Http\Controllers\ClientController@create')->name('client.create');
-
 
 Route::resource('address', '\CustomerManagement\Http\Controllers\AddressController');
 Route::get('adresses/{idClient}', '\CustomerManagement\Http\Controllers\AddressController@index');
 Route::get('address/create/{idClient}', '\CustomerManagement\Http\Controllers\AddressController@create');
 Route::post('address/client/{idClient}', '\CustomerManagement\Http\Controllers\AddressController@store')->name('address.client');
 Route::delete('address/{id}/{idClient}', '\CustomerManagement\Http\Controllers\AddressController@destroy')->name('address.destroy');
-//Route::get('address/{id}','\CustomerManagement\Http\Controllers\AddressController@show');
-//Route::put('address/{id}','\CustomerManagement\Http\Controllers\AddressController@update');
 
